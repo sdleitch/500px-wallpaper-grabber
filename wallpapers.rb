056409885581e10d36a3530ab5115ddd8e871bc8
Scott Leitch <s.d.leitch@gmail.com>
2015-12-09 15:44:10 -0500

    everything seems to work.

---
+++ wallpapers.rb
@@ -0,0 +1,28 @@
+require 'f00px'
+require 'json'
+require 'open-uri'
+
+# Setup F00px
+F00px.configure do |config|
+  config.consumer_key = YOUR_CONSUMER_KEY
+  config.consumer_secret = YOUR_CONSUMER_SECRET
+end
+
+def get_new_wallpapers(pic_array, min_width)
+  to_write = []
+  pic_array.each do |pic|
+    # If photo width is larger the min_width (screen width), put it in an array to be saved
+    to_write << pic if (pic['width'] >= min_width) && (pic['width'] > pic['height']) && (pic['height'] > min_width * 1.6)
+  end
+  # Delete current photos
+  Dir.foreach('wallpapers/') { |f| File.delete(f) if f != '.' && f != '..' }
+  # Save the new photos
+  to_write.each { |pic| File.write("wallpapers/#{pic['id']}.jpg", open(pic['image_url']).read) }
+end
+
+# Make call to get response, parse to JSON, strip to just photo array
+response = F00px.get('photos?feature=popular&image_size=6')
+parsed_response = (JSON.parse(response.body))
+pics = parsed_response['photos']
+
+get_new_wallpapers(pics, 1680) # Run it.
